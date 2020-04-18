413fee51c6be5fe6c545937e50b50223c75ed5b4
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2020-04-18 17:41:37 -0400

    Add traveling distance in game state

--- Scripts/GameState.cs
+++ Scripts/GameState.cs
@@ -4,6 +4,31 @@ using System;
 public class GameState : Node
 {
     public CharacterMovement player;
+    public Station station;
 
     public float fuel { get; set; } = 75;
+
+    public float maxFuel = 125;
+
+    public float traveledDistance = 0.0f;
+
+    public float requiredTravelDistance = 6000.0f;
+
+    private bool destinationReached = false;
+
+    [Signal]
+    public delegate void ReachedDestination();
+
+    public float GetDistanceToTravel() {
+        return Mathf.Clamp(requiredTravelDistance - traveledDistance, 0.0f, requiredTravelDistance);
+    }
+
+    public override void _Process(float delta) {
+        GD.Print("Traveled ", traveledDistance, " meters");
+
+        if(traveledDistance >= requiredTravelDistance && !destinationReached) {
+            EmitSignal(nameof(ReachedDestination));
+            destinationReached = true;
+        }
+    }
 }
