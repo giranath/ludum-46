09839e8947c8d524e5fd79b9008204dd4392e083
Martin <mdaigle299@gmail.com>
2020-04-18 18:28:14 -0400

    Added curse to Station

--- Scripts/Station.cs
+++ Scripts/Station.cs
@@ -3,31 +3,31 @@ using System;
 
 public class Station : Node2D
 {
-    [Export]
-    public Curve fuelToSpeedCurve;
+	[Export]
+	public Curve fuelToSpeedCurve;
 
-    [Export]
-    public float maxSpeed;
+	[Export]
+	public float maxSpeed;
 
-    private GameState gameState;
+	private GameState gameState;
 
-    public bool PropulsorBroken { get; set; } = false;
+	public bool PropulsorBroken { get; set; } = false;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        gameState = GetNode<GameState>("/root/GameState");
-        gameState.station = this;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gameState = GetNode<GameState>("/root/GameState");
+		gameState.station = this;
+	}
 
-    public float GetSpeedAtFuel(float fuel, float maxFuel) {
-        return fuelToSpeedCurve.Interpolate(Mathf.Clamp(fuel / maxFuel, 0.0f, 1.0f)) * maxSpeed;
-    }
+	public float GetSpeedAtFuel(float fuel, float maxFuel) {
+		return fuelToSpeedCurve.Interpolate(Mathf.Clamp(fuel / maxFuel, 0.0f, 1.0f)) * maxSpeed;
+	}
 
-    public override void _Process(float delta) {
-        if(!PropulsorBroken)
-        {
-            gameState.traveledDistance += GetSpeedAtFuel(gameState.fuel, gameState.maxFuel) * delta;
-        }
-    }
+	public override void _Process(float delta) {
+		if(!PropulsorBroken)
+		{
+			gameState.traveledDistance += GetSpeedAtFuel(gameState.fuel, gameState.maxFuel) * delta;
+		}
+	}
 }
