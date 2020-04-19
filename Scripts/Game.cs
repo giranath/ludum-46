2d1a9dab30f14178d18718c562cd3e13ef1cebea
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2020-04-19 16:28:33 -0400

    Add victory/defeat screens

---
+++ Scripts/Game.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class Game : Node2D
+{
+    public override void _Ready()
+    {
+        GameState gameState = GetNode<GameState>("/root/GameState");
+
+        gameState.gameRootNode = GetPath();
+    }
+}
