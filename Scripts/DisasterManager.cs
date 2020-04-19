740c46ebefa8a56975f1930465db779af7976948
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2020-04-19 17:21:39 -0400

    Add screen shake when disaster

--- Scripts/DisasterManager.cs
+++ Scripts/DisasterManager.cs
@@ -13,6 +13,8 @@ public class DisasterManager : Node
 	[Export]
 	public float DisasterCooldownMax = 80.0f;
 
+    [Signal]
+    public delegate void OnDisasterApplied();
 
 	private List<Disaster> Disasters;
 
@@ -71,6 +73,7 @@ public class DisasterManager : Node
 
 		gameState.uiManager.DialogUI.SetText(5, "A disaster happened in the ship", Colors.Red);
 		disaster.Process();
+        EmitSignal(nameof(OnDisasterApplied));
 
 		TimedRepeater = new TimedRepeater(rand.RandfRange(DisasterCooldownMin, DisasterCooldownMax), 1, LaunchDisaster);
 	}
