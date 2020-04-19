a74b3849dddd7bed82fab34d12b8c6c2f1bf5389
Dominic Nault <dnault@rogue-factor.com>
2020-04-18 20:11:17 -0400

    fix powerCore crash

--- Scripts/SmartObjects/PowerCoreSmartObject.cs
+++ Scripts/SmartObjects/PowerCoreSmartObject.cs
@@ -15,6 +15,7 @@ public class PowerCoreSmartObject : SmartObject
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		base._Ready();
 		itemActionMap.Add(itemType.PowerCell, refuel);
 	}
 	
